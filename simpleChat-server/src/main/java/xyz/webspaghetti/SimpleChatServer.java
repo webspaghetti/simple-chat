563d527b6f5710393cb3148b689e38ba8024202d
webspaghetti <ondrakohondra@email.cz>
2025-12-11 21:52:31 +0100

    Add SimpleChatServer.java

---
+++ simpleChat-server/src/main/java/xyz/webspaghetti/SimpleChatServer.java
@@ -0,0 +1,63 @@
+package xyz.webspaghetti;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.nio.channels.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class SimpleChatServer {
+    private static final List<PrintWriter> clientWriters = new ArrayList<>();
+
+    public static void main(String[] args) {
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        try (ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()){
+            serverSocketChannel.bind(new InetSocketAddress(5000));
+
+            while (serverSocketChannel.isOpen()) {
+                SocketChannel clientSocket = serverSocketChannel.accept();
+                PrintWriter writer = new PrintWriter(Channels.newWriter(clientSocket, UTF_8));
+                clientWriters.add(writer);
+                threadPool.submit(new ClientHandler(clientSocket));
+                System.out.println("connection with user established");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void distributeMessage(String message) {
+        for (PrintWriter writer : clientWriters) {
+            writer.println(message);
+            writer.flush();
+        }
+    }
+
+    public static class ClientHandler implements Runnable {
+        BufferedReader reader;
+        SocketChannel socket;
+
+        public ClientHandler(SocketChannel clientSocket) {
+            socket = clientSocket;
+            reader = new BufferedReader(Channels.newReader(socket, UTF_8));
+        }
+
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("read " + "\"" + message + "\"");
+                    distributeMessage(message);
+                }
+            } catch (SocketException e) {
+                // This is the exception you are seeing
+                System.err.println("connection with user lost");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
